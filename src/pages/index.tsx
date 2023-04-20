110de4b38d53c497eca9370662782827cfb97e3d
GiovanySantos <giovany.m.s@hotmail.com>
2023-04-19 23:25:19 -0300

    graphql setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Landing from "./Landing";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 export default function Home() {
   return (
-    <main className='bg-white'>
-      <h1>Hello</h1>
-    </main>
+    <ApolloProvider client={client}>
+      <main className='flex items-center justify-center h-screen bg-white'>
+        <Landing />
+      </main>
+    </ApolloProvider>
   );
 }
 
